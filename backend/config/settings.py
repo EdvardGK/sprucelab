145dce34ed91d9e455cb8cfaaa6d82683d6a9b1a
Edvard Granskogen Kjorstad <ed.subscript@gmail.com>
2026-04-13 15:24:23 +0200

    [auto] backend/config/settings.py updated

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -36,7 +36,10 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-change-this-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv(
+    'ALLOWED_HOSTS',
+    'localhost,127.0.0.1,sprucelab-production.up.railway.app,api.sprucelab.io',
+).split(',')
 
 
 # Application definition
