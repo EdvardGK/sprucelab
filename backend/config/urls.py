fdaf176f3568eff7f6900cc3fa58fe0626e49cab
Edvard Granskogen Kjorstad <ed.subscript@gmail.com>
2026-04-24 19:37:53 +0200

    [auto] backend/config/urls.py updated

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -22,8 +22,6 @@ urlpatterns = [
     path('api/projects/', include('apps.projects.urls')),
     path('api/models/', include('apps.models.urls')),
     path('api/types/', include('apps.entities.urls')),
-    path('api/graph/', include('apps.graph.urls')),
-    # path('api/bep/', include('apps.bep.urls')),  # Archived 2026-04
     path('api/viewers/', include('apps.viewers.urls')),
     path('api/', include('apps.scripting.urls')),
     path('api/automation/', include('apps.automation.urls')),
