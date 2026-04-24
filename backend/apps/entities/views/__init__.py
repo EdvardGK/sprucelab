0461c611eba2d5ac7372925f00576b4424639574
Edvard Granskogen Kjorstad <ed.subscript@gmail.com>
2026-04-25 00:02:21 +0200

    [auto] backend/apps/entities/views/__init__.py updated

---
+++ backend/apps/entities/views/__init__.py
@@ -0,0 +1,36 @@
+"""
+Views package for the entities app.
+
+Re-exports all ViewSets so that urls.py can continue to use:
+    from .views import ProcessingReportViewSet, IFCEntityViewSet, ...
+"""
+from .legacy import ProcessingReportViewSet, IFCEntityViewSet
+from .classification import NS3451CodeViewSet, SemanticTypeViewSet
+from .types import IFCTypeViewSet, TypeMappingViewSet, TypeDefinitionLayerViewSet
+from .materials import MaterialViewSet, MaterialMappingViewSet
+from .typebank import TypeBankEntryViewSet, TypeBankObservationViewSet, TypeBankAliasViewSet
+from .library import (
+    MaterialLibraryViewSet, ProductLibraryViewSet, ProductCompositionViewSet,
+    GlobalTypeLibraryViewSet,
+)
+from .analysis import ModelAnalysisViewSet
+
+__all__ = [
+    'ProcessingReportViewSet',
+    'IFCEntityViewSet',
+    'NS3451CodeViewSet',
+    'SemanticTypeViewSet',
+    'IFCTypeViewSet',
+    'TypeMappingViewSet',
+    'TypeDefinitionLayerViewSet',
+    'MaterialViewSet',
+    'MaterialMappingViewSet',
+    'TypeBankEntryViewSet',
+    'TypeBankObservationViewSet',
+    'TypeBankAliasViewSet',
+    'MaterialLibraryViewSet',
+    'ProductLibraryViewSet',
+    'ProductCompositionViewSet',
+    'GlobalTypeLibraryViewSet',
+    'ModelAnalysisViewSet',
+]
