80468d0e615e0c7575d744db4cbf6f0a5eaa33ff
Edvard Granskogen Kjorstad <ed.subscript@gmail.com>
2026-04-28 14:44:18 -0300

    [auto] backend/apps/projects/urls.py updated

--- backend/apps/projects/urls.py
+++ backend/apps/projects/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProjectViewSet, ProjectConfigViewSet
+from .views import ProjectViewSet, ProjectConfigViewSet, ProjectScopeViewSet
 
 router = DefaultRouter()
 router.register(r'', ProjectViewSet, basename='project')
@@ -8,7 +8,11 @@ router.register(r'', ProjectViewSet, basename='project')
 config_router = DefaultRouter()
 config_router.register(r'', ProjectConfigViewSet, basename='project-config')
 
+scope_router = DefaultRouter()
+scope_router.register(r'', ProjectScopeViewSet, basename='project-scope')
+
 urlpatterns = [
     path('', include(router.urls)),
     path('configs/', include(config_router.urls)),
+    path('scopes/', include(scope_router.urls)),
 ]
