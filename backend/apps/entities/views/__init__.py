750c1c76e6d9b1a7c963975a3ad2b7f2dcf706c2
Edvard Granskogen Kjorstad <ed.subscript@gmail.com>
2026-04-29 00:39:03 -0300

    [auto] backend/apps/entities/views/__init__.py updated

--- backend/apps/entities/views/__init__.py
+++ backend/apps/entities/views/__init__.py
@@ -14,6 +14,7 @@ from .library import (
     GlobalTypeLibraryViewSet,
 )
 from .analysis import ModelAnalysisViewSet
+from .drawings import DrawingSheetViewSet, TitleBlockTemplateViewSet
 
 __all__ = [
     'IFCEntityViewSet',
